5736ca41697e8b8ff956b7909c83570b3f04148d
kyzentyven <kyzentyven@gmail.com>
2025-04-18 02:27:17 +0600

    Added some examples of array indexing operations in JavaScript.

---
+++ index.js
@@ -0,0 +1,16 @@
+const numbers = [1, 35, 22, 66, 8, 7, 9, 11, 21];
+
+console.log(numbers);
+
+// Get element value by index
+console.log(numbers[4]);
+
+const third = numbers[2];
+console.log(third);
+
+// Set or update element value by index
+let abc = 17;
+abc = 66;
+
+numbers[3] = 2;
+console.log(numbers);

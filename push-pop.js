6cbfdc25bfcf9a1e7dccfec670031d8bfe853549
kyzentyven <kyzentyven@gmail.com>
2025-04-18 02:39:35 +0600

    Added some examples of push and pop in JavaScript arrays.

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+const numbers = [1, 3, 5, 7, 9, 11, 13, 15];
+console.log(numbers);
+
+numbers.push(17);
+numbers.push(19);
+numbers.push(21);
+numbers.push(23, 25);
+
+console.log(numbers);
+
+const friends = [Khalid, Ovi, Tawfek, Jim];
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+friends.pop();
+console.log(friends);

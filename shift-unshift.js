c36f24a1be0fbdd5fec0f54eda9858e2be4b7b95
kyzentyven <kyzentyven@gmail.com>
2025-04-18 02:47:20 +0600

    Added some examples of shift and unshift in JavaScript arrays.

---
+++ shift-unshift.js
@@ -0,0 +1,8 @@
+const friends = ['Khalid', 'Ovi', 'Tawfek', 'Mahtab', 'Rafin', 'Nehal'];
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+friends.unshift('Mehedi');
+console.log(friends);

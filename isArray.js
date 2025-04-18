17251360185b5553900bc00740d9de5f82644bbf
kyzentyven <kyzentyven@gmail.com>
2025-04-18 12:10:35 +0600

    Added the concept of isArray in JavaScript arrays.

---
+++ isArray.js
@@ -0,0 +1,12 @@
+const friends = ['Khalid', 'Ovi', 'Tawfek', 'Mahtab', 'Rafin', 'Nehal'];
+
+const numbers = [];
+
+const food = 'T om at o';
+
+const age = 22;
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age));
